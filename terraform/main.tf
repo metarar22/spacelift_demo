f979d05cbae27b67334144128eac43212ce4dd4b
Boris Victoire <120379781+metarar22@users.noreply.github.com>
2025-06-05 15:48:07 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -139,7 +139,7 @@ resource "aws_instance" "demo_instance" {
   }
 }
 
-# Outputs
+# OutputsIP
 output "instance_public_ip" {
   description = "Public IP of the EC2 instance"
   value       = aws_instance.demo_instance.public_ip
